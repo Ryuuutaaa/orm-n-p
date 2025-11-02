829315adc6faba045175bf5d04fcc3ddc4ff9e7c
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-11-03 00:20:13 +0800

    feat: get started

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import { errorMiddleware } from "@/middlewares/error.js";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+dotenv.config({ path: "./.env" });
+
+export const envMode = process.env.NODE_ENV?.trim() || "DEVELOPMENT";
+const port = process.env.PORT || 3000;
+
+const prisa = new PrismaClient();
+const app = express();
+
+app.use(
+  helmet({
+    contentSecurityPolicy: envMode !== "DEVELOPMENT",
+    crossOriginEmbedderPolicy: envMode !== "DEVELOPMENT",
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: " * ", credentials: true }));
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// your routes here
+app.get("/api/new-user", (req, res, next) => {
+  res.status(200).json({
+    success: true, 
+    message: "new user created"
+  })
+})
+
+app.get(/.*/, (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Page not found",
+  });
+});
+
+app.use(errorMiddleware);
+
+app.listen(port, () =>
+  console.log("Server is working on Port:" + port + " in " + envMode + " Mode.")
+);
